60a52553c37c9ec17f16377660f6502e3a43c7d8
Evgeniy Malashenko <111k1808@gmail.com>
2023-10-13 10:14:25 +0300

    finished task

--- src/store/index.js
+++ src/store/index.js
@@ -2,13 +2,30 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    data: [
+      {
+        from: 0,
+        to: 5,
+        audio: 'otkryitie1.mp3',
+        duration: 6
+      },
+
+
+      {
+        from: 6,
+        to: 8,
+        audio: 'video2.mp3',
+        duration: 3
+      },
+      {
+        from: 9,
+        to: 10,
+        audio: 'korotkaya3.mp3',
+        duration: 1
+      },
+    ]
   },
   getters: {
+    getData: state => state.data,
   },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
 })
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,110 @@
+<script setup>
+import { ref, onMounted, computed,  watch } from 'vue';
+import { useStore } from 'vuex';
+
+const store = useStore();
+const data = computed(()=>store.getters.getData);
+const audioRef = ref(new Audio());
+const currentAudioIndex = ref(0);
+const currentTime = ref(0);
+const currentTimeTotal = ref(0);
+const currentTimeSum = ref(0);
+const isPlaying = ref(false);
+const totalDuration = computed(() => {
+  return data.value.reduce((total, audio) => total + audio.duration, 0);
+});
+const playAudio = () => {
+  isPlaying.value = true;
+  audioRef.value.src = require(`@/assets/${data.value[currentAudioIndex.value].audio}`);
+  audioRef.value.currentTime = currentTime.value;
+  audioRef.value.play();
+};
+const pauseAudio = () => {
+  isPlaying.value = false;
+  audioRef.value.pause();
+};
+const stopAudio = () => {
+  isPlaying.value = false;
+  audioRef.value.pause();
+  currentTime.value = 0;
+  audioRef.value.currentTime = 0;
+};
+const seekAudio = () => {
+  if (isPlaying.value) {
+    pauseAudio();
+    playAudio();
+  }
+};
+const clickRange = () =>{
+  let val =  currentTimeTotal.value;
+  for (let i = 0; i < data.value.length; i++) {
+    if(val >= data.value[i].from && val <= data.value[i].to){
+      currentAudioIndex.value = i;
+      currentTime.value = val - data.value[i].from;
+      currentTimeSum.value = data.value[i].from;
+    }
+  }
+}
+const updateCurrentTime = () => {
+  currentTime.value = Math.floor(audioRef.value.currentTime);
+
+  if (currentTime.value >= data.value[currentAudioIndex.value].duration) {
+    if(currentAudioIndex.value == data.value.length-1){
+      pauseAudio();
+    } else {
+      currentAudioIndex.value++;
+      currentTime.value = 0;
+      if (currentAudioIndex.value >= data.value.length) {
+        currentAudioIndex.value = 0;
+      }
+      playAudio();
+    }
+  }
+};
+
+onMounted(() => {
+  audioRef.value.addEventListener('timeupdate', updateCurrentTime);
+});
+
+watch(
+    () => currentAudioIndex.value,
+    (val) => {
+        for (let i = 0; i < val; i++) {
+          currentTimeSum.value += data.value[i].duration;
+        }
+      currentTimeTotal.value = currentTimeSum.value;
+    }
+)
+
+watch(
+    () => currentTime.value,
+    (val) => {
+     currentTimeTotal.value = val + currentTimeSum.value;
+    }
+)
+
+watch(
+    () => currentTimeTotal.value,
+    (val) => {
+      if(val>=totalDuration.value){
+        currentTimeTotal.value = totalDuration.value;
+      }
+    }
+)
+</script>
 <template>
-  <div class="home">
-  hhhhh
+  <div class="audio-player">
+    <input type="range" min="0" :max="totalDuration" v-model="currentTimeTotal" @click="clickRange" @input="seekAudio" />
+    <div>
+      <button @click="playAudio">Play</button>
+      <button @click="pauseAudio">Pause</button>
+      <button @click="stopAudio">Stop</button>
+    </div>
+    <p>Time: {{ currentTimeTotal }} seconds</p>
   </div>
 </template>
-
-<script>
-</script>
+<style scoped>
+.audio-player {
+  text-align: center;
+}
+</style>
